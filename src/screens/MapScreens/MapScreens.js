0f46b802ae99e0a5b66d7b7c4ddd8b28236fd1cf
Famil <frostgamingrespawn@gmail.com>
2022-05-02 13:57:15 +0400

    refactored map screen

--- src/screens/MapScreens/MapScreens.js
+++ src/screens/MapScreens/MapScreens.js
@@ -2,6 +2,7 @@ import React,{useEffect, useState} from 'react';
 import {View, Text, Image} from 'react-native';
 import MapView, {Marker} from 'react-native-maps';
 import api from '../../api/api';
+import CustomMarker from '../../components/CustomMarker/CustomMarker';
 import ShortenLabelOfHouse from '../../components/ShortenLabelOfHouse/ShortenLabelOfHouse';
 import styles from './MapScreens.styles';
 
@@ -18,17 +19,19 @@ const MapScreens  = () => {
     }, [])
 
     return (
-        <View style={{flex: 1}}>
-            <MapView style={{flex: 1, zIndex: 100}}>
+        <View style={style.mainView}>
+            <MapView style={style.mapView}>
             {allHouses.map((marker, index) => (
                 <Marker
                     key={marker.id}
                     coordinate={{latitude : marker.location.lat , longitude : marker.location.lng}}
                     onPress={() => setChosenHouse(marker)}
                 >
-                    <View style={style.markerView}>
-                        <Text style={{color: 'white'}}>{marker.city_id}$</Text>
-                    </View>
+                    <CustomMarker
+                        chosenHouse={chosenHouse}
+                        marker={marker}
+                        text={marker.city_id}
+                    />
                 </Marker>
             ))}
             </MapView>
@@ -39,6 +42,7 @@ const MapScreens  = () => {
                         distance={chosenHouse.distance}
                         image={chosenHouse.images[0].url}
                         price={chosenHouse.city_id}
+                        rating={'4.5'}
                     />
                 )
             }
--- src/screens/MapScreens/MapScreens.styles.js
+++ src/screens/MapScreens/MapScreens.styles.js
@@ -4,13 +4,11 @@ import { StyleSheet } from 'react-native';
 const styles = () => {
   
     return StyleSheet.create({
-        markerView: {
-            zIndex: 1000,
-            width: 35, 
-            height: 35, 
-            backgroundColor: '#4b4b4d', 
-            alignItems: 'center', 
-            justifyContent: 'center'
+        mainView: {
+            flex: 1
+        },
+        mapView: {
+            flex: 1
         }
     });
   };
